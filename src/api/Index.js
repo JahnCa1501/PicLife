72ec4c70914bf600c7be0fd77996e9414ad93dab
Yesica <nicolisaula2000@gmail.com>
2021-07-31 14:48:56 -0600

    pantalla Home

--- src/api/Index.js
+++ src/api/Index.js
@@ -3,6 +3,8 @@
 const api = "https://api.pexels.com/v1/curated";
 const apiKey = "563492ad6f91700001000001bef0c946941147b298b3710bb8e84838";
 
+
+
 const curatedPicture = async () => {
    
     const response = await fetch(api, {
